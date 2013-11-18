1f609946c12ceb38f7a9c8ff94df9775d2ed3a08
Becca Bainbridge <beccabainbridge@gmail.com>
2013-11-18 11:01:59 -0500

    finally removed bad importing

--- quizgame.py
+++ quizgame.py
@@ -5,7 +5,7 @@ from contextlib import closing
 from random import shuffle
 from flask import Flask, render_template, request, redirect, session, flash
 from flaskext.bcrypt import Bcrypt
-from access_db import * # don't do this :)
+import access_db
 
 #configs
 DEBUG = os.environ["QUIZ_DEBUG"]
@@ -22,11 +22,11 @@ class QuestionValidationError(Exception):
     pass
 
 def get_questions(n, ordered=False):
-    question_nums = get_question_nums()
+    question_nums = access_db.get_question_nums()
     if not ordered:
         shuffle(question_nums)
     question_nums = question_nums[:n]
-    questions = [get_question(num) for num in question_nums]
+    questions = [access_db.get_question(num) for num in question_nums]
     return questions
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -34,8 +34,8 @@ def login():
     error = None
     if request.method == 'POST':
         username = request.form['username']
-        if username in get_usernames():
-            pw_hash = get_password(username)
+        if username in access_db.get_usernames():
+            pw_hash = access_db.get_password(username)
             if bcrypt.check_password_hash(pw_hash, request.form['password']):
                 session['username'] = username
                 session['logged_in'] = True
@@ -53,7 +53,7 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect('/')
-        
+
 @app.route('/create_account', methods=['GET', 'POST'])
 def create_user():
     error = None
@@ -63,16 +63,16 @@ def create_user():
         password_confirm = request.form['password_confirm']
         if password != password_confirm:
             error = "Passwords don't match"
-        elif username in get_usernames():
+        elif username in access_db.get_usernames():
             error = "Username already in use. Please choose another."
         else:
             pw_hash = bcrypt.generate_password_hash(password)
-            add_user(username, pw_hash)
+            access_db.add_user(username, pw_hash)
             flash('Account created')
             return redirect('login')
 
     return render_template('create_account.html', error=error)
-    
+
 @app.route('/database', methods=['GET', 'POST'])
 def database_access():
     if request.method == 'POST':
@@ -81,29 +81,29 @@ def database_access():
         q = request.form['question']
         a1, a2, a3, a4 = request.form['ans1'], request.form['ans2'], request.form['ans3'], request.form['ans4']
         c = request.form['correct']
-        
+
         question_info = [q, a1, a2, a3, a4, c]
         names = ['question', 'ans1', 'ans2', 'ans3', 'ans4', 'correct']
-                
+
         try:
             if not i and (update_type == 'delete' or update_type == 'update'):
                 raise QuestionValidationError('Must enter question number to update or delete')
-            
+
             if update_type == 'delete':
-                add_proposed([i] + question_info + ['delete', session['username']])
-                flash('Question submitted for deletion')                    
+                access_db.add_proposed([i] + question_info + ['delete', session['username']])
+                flash('Question submitted for deletion')
             else:
                 for item in question_info:
                     if item == "" and update_type == 'add':
                         raise QuestionValidationError('Input cannot be left blank')
 
-                add_proposed([i] + question_info + [update_type, session['username']])
+                access_db.add_proposed([i] + question_info + [update_type, session['username']])
                 flash('Question submitted for %s' % update_type)
 
         except (sqlite3.OperationalError, QuestionValidationError) as e:
             flash('Invalid question entry: ' + str(e))
 
-    questions = get_questions(get_num_questions(), ordered=True)
+    questions = access_db.get_questions(get_num_questions(), ordered=True)
     return render_template('database.html', questions=questions)
 
 @app.route('/admin', methods=['GET','POST'])
@@ -113,18 +113,18 @@ def admin():
         action = request.form['button']
         for change in changes:
             if action == 'Reject':
-                remove_proposed(change)
+                access_db.remove_proposed(change)
             elif action == 'Accept':
-                make_db_change(change)
+                access_db.make_db_change(change)
 
-    additions, updates, deletions = get_proposed()
-    questions = get_questions(get_num_questions(), ordered=True)
+    additions, updates, denletions = access_db.get_proposed()
+    questions = access_db.get_questions(access_db.get_num_questions(), ordered=True)
     return render_template('admin.html', add=additions, update=updates, delete=deletions, questions=questions)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        total_questions = get_num_questions()
+        total_questions = access_db.get_num_questions()
         options = range(5, total_questions + 1, 5)
 
         if options[-1] != total_questions:
@@ -139,12 +139,12 @@ def index():
         app.curquestion = 0
         app.score = 0
         num_to_show = 10
-        app.highscores = get_highscores(num_to_show)
+        app.highscores = access_db.get_highscores(num_to_show)
         if len(app.highscores) >= num_to_show:
             app.lowest_highscore = app.highscores[-1][1]
         else:
             app.lowest_highscore = None
-        
+
         return redirect('main')
 
 @app.route('/main')
@@ -162,10 +162,10 @@ def end():
         get_info = True
     elif request.method == 'POST':
         name = request.form['name']
-        add_to_highscores(name, score)
+        access_db.add_to_highscores(name, score)
         # update highscores
-        app.highscores = get_highscores(10)
-                
+        app.highscores = access_db.get_highscores(10)
+
     return render_template('end.html', score=score, \
                                highscores=app.highscores, get_info=get_info)
 
@@ -181,7 +181,7 @@ def next():
             return redirect('main')
         else:
             ans = ans['response']
-        
+
         app.curquestion += 1
 
         correct = app.question_info['correct']
@@ -196,6 +196,6 @@ def next():
         return redirect('main')
 
 if __name__ == '__main__':
-    if get_num_questions() == 0:
-        create_database(DATABASE, SCHEMA, CSV)
+    if access_db.get_num_questions() == 0:
+        access_db.create_database(DATABASE, SCHEMA, CSV)
     app.run()
